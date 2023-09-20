de06479e47b75da84f9f44c8818892b97bee8558
Joost Schuur <jschuur@jschuur.com>
2023-09-20 14:15:32 +0800

    Align loading animation color with theme

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ import ScaleLoader from 'react-spinners/ScaleLoader';
 export default function Loading() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <ScaleLoader color='#36d7b7' />
+      <ScaleLoader color='#2563EB' />
       <p className='text-gray-500 text-sm font-light mt-2'>Loading...</p>
     </div>
   );
