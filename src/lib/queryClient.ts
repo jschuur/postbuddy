7936581ce104df425aec0873ef739cd39d81e9c9
Joost Schuur <jschuur@jschuur.com>
2023-09-21 08:33:42 +0800

    Set staleTime to 30 mins

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -6,7 +6,7 @@ const getQueryClient = cache(
     new QueryClient({
       defaultOptions: {
         queries: {
-          refetchOnWindowFocus: false,
+          staleTime: 1000 * 60 * 30,
         },
       },
     })
